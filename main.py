cbd7d654c4b82e7bfd613eecf5b57dc13cf89591
duc <hmduc227@gmail.com>
2023-03-17 17:43:45 +0700

    update cocodataset path

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def get_args_parser():
 
     # dataset parameters
     parser.add_argument('--dataset_file', default='coco')
-    parser.add_argument('--coco_path', default='./data/coco', type=str)
+    parser.add_argument('--coco_path', default='../coco-dataset', type=str)
     parser.add_argument('--coco_panoptic_path', type=str)
     parser.add_argument('--remove_difficult', action='store_true')
 
