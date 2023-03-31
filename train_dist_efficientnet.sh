5399dbd84f1c47b6c312d462061edd8408c49806
duc <hmduc227@gmail.com>
2023-04-01 02:25:37 +0700

    config train script

---
+++ train_dist_efficientnet.sh
@@ -0,0 +1,1 @@
+GPUS_PER_NODE=2 ./tools/run_dist_launch.sh 2 ./train_efficientnet.sh
--- train_efficientnet.sh
+++ train_efficientnet.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+
 python -u main.py \
     --num_feature_levels 1 \
     --output_dir "exps/effi_v2s_deformable_detr" \
     --backbone "efficientnet" \
     --batch_size 2 \
-    --device "cuda:1" \
-    --seed 10
+    --lr 1e-4 \
+    --lr_backbone 1e-5
